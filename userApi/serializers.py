c3343acc162019ba6edc37e293389584dbf520b2
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-13 10:37:07 +0530

    serializer.py created

---
+++ userApi/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from userApi.models import User
+class UserRegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = User
+
+        fields = ['username','password','email']
