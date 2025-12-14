8c0bdf2c619eabbb0dd18fe9b7ddb770d9c92f7e
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-14 12:40:09 +0530

    aergt

--- userApi/views.py
+++ userApi/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from userApi.serializers import UserRegisterSerializer
+from rest_framework.views import APIView
+from rest_framework import status
+from userApi.models import *
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 # Create your views here.
+
+
+class UserRegisterView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+
+        # vanna data ne edthu
+
+        user_serialzer = UserRegisterSerializer(data = request.data)
+
+        if user_serialzer.is_valid():
+
+            user_serialzer.save()
+
+            
